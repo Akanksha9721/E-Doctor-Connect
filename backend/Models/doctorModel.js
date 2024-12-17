a97cacfe332f17cde2b075cbd06b6ba35cff37e8
Akanksha97 <akankshayadav8467@gmail.com>
2024-12-17 10:53:10 +0530

    models

---
+++ backend/Models/doctorModel.js
@@ -0,0 +1,18 @@
+const { model, Schema, } = require('../connection');
+
+const mySchema = new Schema({
+    name: String,
+    email: { type: String, unique: true },
+    password: { type: String, require: true },
+    contact: { type: String },
+    qualification: { type: String },
+    specilization: { type: String },
+    experience: { type: Number },
+    fees: { type: Number },
+    avalilablity: { type: Date, default: Date.now },
+    avatar: { type: String },
+    city: { type: String, default: 'Unknown' },
+
+});
+
+module.exports = model('doctors', mySchema);
