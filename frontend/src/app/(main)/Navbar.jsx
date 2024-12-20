7bd5a84a9b2419821581a4c70fdb3ef5b1100d13
Akanksha97 <akankshayadav8467@gmail.com>
2024-12-20 11:06:19 +0530

    dropdown

--- frontend/src/app/(main)/Navbar.jsx
+++ frontend/src/app/(main)/Navbar.jsx
@@ -1,197 +1,191 @@
-import React from 'react'
+'use client'
+import Link from 'next/link'
+import React, { useState } from 'react'
+
+
 
 const Navbar = () => {
+  const [isDropdownOpen, setDropdownOpen] = useState(false);
+  const [isMenuOpen, setMenuOpen] = useState(false);
+
+  const toggleDropdown = () => setDropdownOpen(!isDropdownOpen);
+  const toggleMenu = () => setMenuOpen(!isMenuOpen);
+
+
   return (
-    <>
-      <header className="flex flex-wrap md:justify-start md:flex-nowrap z-50 w-full bg-blue-600">
-        <nav className="relative max-w-[66rem] w-full md:flex md:items-center md:justify-between md:gap-3 mx-auto px-4 sm:px-6 lg:px-8 py-2">
-          {/* Logo w/ Collapse Button */}
-          <div className="flex items-center justify-between">
-            <a
-              className="flex-none font-semibold text-xl text-white focus:outline-none focus:opacity-80"
-              href="#"
-              aria-label="Brand"
-            >
-              E-Doctor 
-            </a>
-            {/* Collapse Button */}
-            <div className="md:hidden">
-              <button
-                type="button"
-                className="hs-collapse-toggle relative size-9 flex justify-center items-center text-sm font-semibold rounded-lg border border-white/50 text-white hover:bg-white/10 focus:outline-none focus:bg-white/10 disabled:opacity-50 disabled:pointer-events-none"
-                id="hs-base-header-collapse"
-                aria-expanded="false"
-                aria-controls="hs-base-header"
-                aria-label="Toggle navigation"
-                data-hs-collapse="#hs-base-header"
-              >
-                <svg
-                  className="hs-collapse-open:hidden size-4"
-                  xmlns="http://www.w3.org/2000/svg"
-                  width={24}
-                  height={24}
-                  viewBox="0 0 24 24"
-                  fill="none"
-                  stroke="currentColor"
-                  strokeWidth={2}
-                  strokeLinecap="round"
-                  strokeLinejoin="round"
-                >
-                  <line x1={3} x2={21} y1={6} y2={6} />
-                  <line x1={3} x2={21} y1={12} y2={12} />
-                  <line x1={3} x2={21} y1={18} y2={18} />
-                </svg>
-                <svg
-                  className="hs-collapse-open:block shrink-0 hidden size-4"
-                  xmlns="http://www.w3.org/2000/svg"
-                  width={24}
-                  height={24}
-                  viewBox="0 0 24 24"
-                  fill="none"
-                  stroke="currentColor"
-                  strokeWidth={2}
-                  strokeLinecap="round"
-                  strokeLinejoin="round"
-                >
-                  <path d="M18 6 6 18" />
-                  <path d="m6 6 12 12" />
-                </svg>
-                <span className="sr-only">Toggle navigation</span>
-              </button>
-            </div>
-            {/* End Collapse Button */}
-          </div>
-          {/* End Logo w/ Collapse Button */}
-          {/* Collapse */}
-          <div
-            id="hs-base-header"
-            className="hs-collapse hidden overflow-hidden transition-all duration-300 basis-full grow md:block"
-            aria-labelledby="hs-base-header-collapse"
+    <div>
+      <nav className="bg-white border-gray-200 dark:bg-gray-900 dark:border-gray-700">
+        <div className="max-w-screen-xl flex flex-wrap items-center justify-between mx-auto p-4">
+          <Link href="#" className="flex items-center space-x-3 rtl:space-x-reverse">
+            <img
+              src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRFA2bos1wDKWuwecD7EE5WtLQYEyGxCq3DGQ&s"
+              className="h-8"
+              alt="Flowbite Logo"
+            />
+            <span className="self-center text-2xl font-semibold whitespace-nowrap dark:text-white">
+              E-Doctor
+            </span>
+          </Link>
+          <button
+           onClick={toggleMenu}
+            data-collapse-toggle="navbar-dropdown"
+            type="button"
+            className="inline-flex items-center p-2 w-10 h-10 justify-center text-sm text-gray-500 rounded-lg md:hidden hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-gray-200 dark:text-gray-400 dark:hover:bg-gray-700 dark:focus:ring-gray-600"
+            aria-controls="navbar-dropdown"
+            aria-expanded="false"
           >
-            <div className="overflow-hidden overflow-y-auto max-h-[75vh] [&::-webkit-scrollbar]:w-2 [&::-webkit-scrollbar-thumb]:rounded-full [&::-webkit-scrollbar-track]:bg-gray-100 [&::-webkit-scrollbar-thumb]:bg-gray-300 dark:[&::-webkit-scrollbar-track]:bg-neutral-700 dark:[&::-webkit-scrollbar-thumb]:bg-neutral-500">
-              <div className="py-2 md:py-0 flex flex-col md:flex-row md:items-center md:justify-end gap-0.5 md:gap-1">
-                <a
-                  className="p-2 flex items-center text-sm text-white focus:outline-none focus:text-white"
-                  href="/doctor-register"
+            <span className="sr-only">Open main menu</span>
+            <svg
+              className="w-5 h-5"
+              aria-hidden="true"
+              xmlns="http://www.w3.org/2000/svg"
+              fill="none"
+              viewBox="0 0 17 14"
+            >
+              <path
+                stroke="currentColor"
+                strokeLinecap="round"
+                strokeLinejoin="round"
+                strokeWidth={2}
+                d="M1 1h15M1 7h15M1 13h15"
+              />
+            </svg>
+          </button>
+          <div className={`${isMenuOpen ? 'block' : 'hidden'} w-full md:block md:w-auto`}
+          id="navbar-dropdown">
+            <ul className="flex flex-col font-medium p-4 md:p-0 mt-4 border border-gray-100 rounded-lg bg-gray-50 md:space-x-8 rtl:space-x-reverse md:flex-row md:mt-0 md:border-0 md:bg-white dark:bg-gray-800 md:dark:bg-gray-900 dark:border-gray-700">
+              <li>
+                <Link
+                  href="#"
+                  className="block py-2 px-3 text-white bg-blue-700 rounded md:bg-transparent md:text-blue-700 md:p-0 md:dark:text-blue-500 dark:bg-blue-600 md:dark:bg-transparent"
                   aria-current="page"
                 >
-                  <svg
-                    className="shrink-0 size-4 me-3 md:me-2 block md:hidden"
-                    xmlns="http://www.w3.org/2000/svg"
-                    width={24}
-                    height={24}
-                    viewBox="0 0 24 24"
-                    fill="none"
-                    stroke="currentColor"
-                    strokeWidth={2}
-                    strokeLinecap="round"
-                    strokeLinejoin="round"
-                  >
-                    <path d="M15 21v-8a1 1 0 0 0-1-1h-4a1 1 0 0 0-1 1v8" />
-                    <path d="M3 10a2 2 0 0 1 .709-1.528l7-5.999a2 2 0 0 1 2.582 0l7 5.999A2 2 0 0 1 21 10v9a2 2 0 0 1-2 2H5a2 2 0 0 1-2-2z" />
-                  </svg>
-                  Register
-                </a>
-                <a
-                  className="p-2 flex items-center text-sm text-white/80 hover:text-white focus:outline-none focus:text-white"
-                  href="/doctor-login"
+                  Home
+                </Link>
+              </li>
+              <li>
+                <button
+                onClick={toggleDropdown}
+                  id="dropdownNavbarLink"
+                  data-dropdown-toggle="dropdownNavbar"
+                  className="flex items-center justify-between w-full py-2 px-3 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 md:w-auto dark:text-white md:dark:hover:text-blue-500 dark:focus:text-white dark:border-gray-700 dark:hover:bg-gray-700 md:dark:hover:bg-transparent"
                 >
+                  Register{" "}
                   <svg
-                    className="shrink-0 size-4 me-3 md:me-2 block md:hidden"
+                    className="w-2.5 h-2.5 ms-2.5"
+                    aria-hidden="true"
                     xmlns="http://www.w3.org/2000/svg"
-                    width={24}
-                    height={24}
-                    viewBox="0 0 24 24"
                     fill="none"
-                    stroke="currentColor"
-                    strokeWidth={2}
-                    strokeLinecap="round"
-                    strokeLinejoin="round"
+                    viewBox="0 0 10 6"
                   >
-                    <path d="M19 21v-2a4 4 0 0 0-4-4H9a4 4 0 0 0-4 4v2" />
-                    <circle cx={12} cy={7} r={4} />
-                  </svg>
-                  login
-                </a>
-                <a
-                  className="p-2 flex items-center text-sm text-white/80 hover:text-white focus:outline-none focus:text-white"
-                  href="#"
-                >
-                  <svg
-                    className="shrink-0 size-4 me-3 md:me-2 block md:hidden"
-                    xmlns="http://www.w3.org/2000/svg"
-                    width={24}
-                    height={24}
-                    viewBox="0 0 24 24"
-                    fill="none"
-                    stroke="currentColor"
-                    strokeWidth={2}
-                    strokeLinecap="round"
-                    strokeLinejoin="round"
-                  >
-                    <path d="M12 12h.01" />
-                    <path d="M16 6V4a2 2 0 0 0-2-2h-4a2 2 0 0 0-2 2v2" />
-                    <path d="M22 13a18.15 18.15 0 0 1-20 0" />
-                    <rect width={20} height={14} x={2} y={6} rx={2} />
-                  </svg>
-                  
-                </a>
-                <a
-                  className="p-2 flex items-center text-sm text-white/80 hover:text-white focus:outline-none focus:text-white"
-                  href="#"
-                >
-                  <svg
-                    className="shrink-0 size-4 me-3 md:me-2 block md:hidden"
-                    xmlns="http://www.w3.org/2000/svg"
-                    width={24}
-                    height={24}
-                    viewBox="0 0 24 24"
-                    fill="none"
-                    stroke="currentColor"
-                    strokeWidth={2}
-                    strokeLinecap="round"
-                    strokeLinejoin="round"
-                  >
-                    <path d="M4 22h16a2 2 0 0 0 2-2V4a2 2 0 0 0-2-2H8a2 2 0 0 0-2 2v16a2 2 0 0 1-2 2Zm0 0a2 2 0 0 1-2-2v-9c0-1.1.9-2 2-2h2" />
-                    <path d="M18 14h-8" />
-                    <path d="M15 18h-5" />
-                    <path d="M10 6h8v4h-8V6Z" />
-                  </svg>
-                  Blog
-                </a>
-                {/* Button Group */}
-                <div className="relative flex flex-wrap items-center gap-x-1.5 md:ps-2.5 mt-1 md:mt-0 md:ms-1.5 before:block before:absolute before:top-1/2 before:-start-px before:w-px before:h-4 before:bg-white/30 before:-translate-y-1/2">
-                  <a
-                    className="p-2 w-full flex items-center text-sm text-white/80 hover:text-white focus:outline-none focus:text-white"
-                    href="#"
-                  >
-                    <svg
-                      className="shrink-0 size-4 me-3 md:me-2"
-                      xmlns="http://www.w3.org/2000/svg"
-                      width={24}
-                      height={24}
-                      viewBox="0 0 24 24"
-                      fill="none"
+                    <path
                       stroke="currentColor"
-                      strokeWidth={2}
                       strokeLinecap="round"
                       strokeLinejoin="round"
-                    >
-                      <path d="M19 21v-2a4 4 0 0 0-4-4H9a4 4 0 0 0-4 4v2" />
-                      <circle cx={12} cy={7} r={4} />
-                    </svg>
-                    Log in
-                  </a>
+                      strokeWidth={2}
+                      d="m1 1 4 4 4-4"
+                    />
+                  </svg>
+                </button>
+                {/* Dropdown menu */}
+                <div
+                  id="dropdownNavbar"
+                  className={`${
+                    isDropdownOpen ? 'block' : 'hidden'
+                  } z-10 font-normal bg-white divide-y divide-gray-100 rounded-lg shadow w-44 dark:bg-gray-700 dark:divide-gray-600`}
+                >
+                  <ul
+                    className="py-2 text-sm text-gray-700 dark:text-gray-400"
+                    aria-labelledby="dropdownLargeButton"
+                  >
+                    <li>
+                      <Link
+                        href="/doctor-register"
+                        className="block px-4 py-2 hover:bg-gray-100 dark:hover:bg-gray-600 dark:hover:text-white"
+                      >
+                        doctor 
+                      </Link>
+                    </li>
+                    <li>
+                      <Link
+                        href="/user-register"
+                        className="block px-4 py-2 hover:bg-gray-100 dark:hover:bg-gray-600 dark:hover:text-white"
+                      >
+                        User
+                      </Link>
+                    </li>
+                  </ul>
+                
                 </div>
-                {/* End Button Group */}
-              </div>
-            </div>
+              </li>
+              <li>
+                <button
+                onClick={toggleDropdown}
+                  id="dropdownNavbarLink"
+                  data-dropdown-toggle="dropdownNavbar"
+                  className="flex items-center justify-between w-full py-2 px-3 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 md:w-auto dark:text-white md:dark:hover:text-blue-500 dark:focus:text-white dark:border-gray-700 dark:hover:bg-gray-700 md:dark:hover:bg-transparent"
+                >
+                  Login{" "}
+                  <svg
+                    className="w-2.5 h-2.5 ms-2.5"
+                    aria-hidden="true"
+                    xmlns="http://www.w3.org/2000/svg"
+                    fill="none"
+                    viewBox="0 0 10 6"
+                  >
+                    <path
+                      stroke="currentColor"
+                      strokeLinecap="round"
+                      strokeLinejoin="round"
+                      strokeWidth={2}
+                      d="m1 1 4 4 4-4"
+                    />
+                  </svg>
+                </button>
+                {/* Dropdown menu */}
+                <div
+                  id="dropdownNavbar"
+                  className={`${
+                    isDropdownOpen ? 'block' : 'hidden'
+                  } z-10 font-normal bg-white divide-y divide-gray-100 rounded-lg shadow w-44 dark:bg-gray-700 dark:divide-gray-600`}
+                >
+                  <ul
+                    className="py-2 text-sm text-gray-700 dark:text-gray-400"
+                    aria-labelledby="dropdownLargeButton"
+                  >
+                    <li>
+                      <Link
+                        href="/doctor-login"
+                        className="block px-4 py-2 hover:bg-gray-100 dark:hover:bg-gray-600 dark:hover:text-white"
+                      >
+                        doctor
+                      </Link>
+                    </li>
+                    <li>
+                      <Link
+                        href="/user-login"
+                        className="block px-4 py-2 hover:bg-gray-100 dark:hover:bg-gray-600 dark:hover:text-white"
+                      >
+                        User 
+                      </Link>
+                    </li>
+                  </ul>
+                
+                </div>
+              </li>
+              <li>
+                <Link
+                  href="/browse-doctor"
+                  className="block py-2 px-3 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 dark:text-white md:dark:hover:text-blue-500 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent"
+                >
+                  Dotors
+                </Link>
+              </li>
+            </ul>
           </div>
-          {/* End Collapse */}
-        </nav>
-      </header>
-    </>
-
+        </div>
+      </nav>
+    </div>
   )
 }
 
