7203509dac6940acd0af33777c55253b9ed4ad1a
Akanksha97 <akankshayadav8467@gmail.com>
2025-05-18 14:11:28 +0530

    front

--- frontend/eslint.config.mjs
+++ frontend/eslint.config.mjs
@@ -9,6 +9,19 @@ const compat = new FlatCompat({
   baseDirectory: __dirname,
 });
 
-const eslintConfig = [...compat.extends("next/core-web-vitals")];
+const eslintConfig = [
+  ...compat.extends("next/core-web-vitals"),
+  {
+    rules: {
+      "react/no-unescaped-entities": "off",
+      "react/display-name": "off",
+      "@next/next/no-img-element": "off",
+      "react/no-unknown-property": [
+        "error",
+        { ignore: ["jsx", "global"] }
+      ]
+    }
+  }
+];
 
 export default eslintConfig;
