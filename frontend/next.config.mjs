c74bb7ea0e6fc5c602eac635db662e8b105fc9ad
Akanksha97 <akankshayadav8467@gmail.com>
2025-05-21 23:39:31 +0530

    next

--- frontend/next.config.mjs
+++ frontend/next.config.mjs
@@ -4,7 +4,7 @@ const nextConfig = {
     unoptimized: true, // disables built-in Next.js image optimization
   },
   reactStrictMode: true, // helps catch potential problems in development
-  swcMinify: true, // enables SWC compiler-based minification
+  //swcMinify: true, // enables SWC compiler-based minification
 }
 
 export default nextConfig;
