ad278c09355c3471d00f59ede60a36c356a2c319
Akanksha97 <akankshayadav8467@gmail.com>
2025-05-22 10:57:37 +0530

    change

--- frontend/src/app/page.jsx
+++ frontend/src/app/page.jsx
@@ -488,7 +488,7 @@ export default function Page() {
             </p>
           </div>
 
-          <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 gap-6">
+          <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 gap-6 ">
             {[
               {
                 title: "Good Healthcare Practices",
